695436083a8f75b9fcea94a057a7722e303d8d35
khuaho <hokhua3@gmail.com>
2023-08-18 09:12:59 +0700

    Move get current position from splash page to main page

--- lib/modules/main/main_page.dart
+++ lib/modules/main/main_page.dart
@@ -1,28 +1,46 @@
 import 'dart:async';
 
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:geolocator/geolocator.dart';
 
+import '../../app/providers/app_settings_provider.dart';
 import '../../global/app_router/app_router.dart';
+import '../../global/services/geolocator_service.dart';
 import '../../global/themes/app_colors.dart';
 
 @RoutePage()
-class MainPage extends StatefulWidget {
+class MainPage extends ConsumerStatefulWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
-  State<MainPage> createState() => _MainPageState();
+  ConsumerState<MainPage> createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends ConsumerState<MainPage> {
   late final StreamSubscription changePageSubscription,
       loadBadgeMailboxSubscription;
   TabsRouter? tabsRouter;
-  int pageIndex = 0;
+  LocationSettings? locationSettings;
+  StreamSubscription<Position>? positionStream;
 
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      setLocationSettings();
+      await getAndSaveCurrentPosition();
+    });
+    positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position? position) {
+      final appSettingsPro = ref.read(appSettingProvider.notifier);
+      if (position != null) {
+        appSettingsPro.saveCurrentPosition(position);
+      }
+    });
   }
 
   @override
@@ -30,6 +48,48 @@ class _MainPageState extends State<MainPage> {
     changePageSubscription.cancel();
     loadBadgeMailboxSubscription.cancel();
     super.dispose();
+  }
+
+  Future<void> getAndSaveCurrentPosition() async {
+    Position? currentPosition = await GeolocatorService.determinePosition();
+    final appSettingsPro = ref.read(appSettingProvider.notifier);
+    if (currentPosition != null) {
+      appSettingsPro.saveCurrentPosition(currentPosition);
+    }
+  }
+
+  void setLocationSettings() {
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      locationSettings = AndroidSettings(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: 100,
+        forceLocationManager: true,
+        intervalDuration: const Duration(seconds: 10),
+        //(Optional) Set foreground notification config to keep the app alive
+        //when going to the background
+        foregroundNotificationConfig: const ForegroundNotificationConfig(
+          notificationText:
+              "Example app will continue to receive your location even when you aren't using it",
+          notificationTitle: 'Running in Background',
+          enableWakeLock: true,
+        ),
+      );
+    } else if (defaultTargetPlatform == TargetPlatform.iOS ||
+        defaultTargetPlatform == TargetPlatform.macOS) {
+      locationSettings = AppleSettings(
+        accuracy: LocationAccuracy.high,
+        activityType: ActivityType.fitness,
+        distanceFilter: 100,
+        pauseLocationUpdatesAutomatically: true,
+        // Only set to true if our app will be started up in the background.
+        showBackgroundLocationIndicator: false,
+      );
+    } else {
+      locationSettings = const LocationSettings(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: 100,
+      );
+    }
   }
 
   final routes = [
